d2b16b9eec512352a6c39dc451258f62fc283d63
Code Sandbox <hello@codesandbox.io>
2023-05-02 06:11:55 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e100ec2c0292dcc54f7c8c18923d2467.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1832a759bb3fbbe2d5d1278e0d3f5c3",
+    "url": "https://shahroz869.github.io/csb-c0oogp/index.html"
+  },
+  {
+    "revision": "2e3f48364264bcc07cb9",
+    "url": "https://shahroz869.github.io/csb-c0oogp/static/css/main.462b0bd9.chunk.css"
+  },
+  {
+    "revision": "3ad784398ec6227b399a",
+    "url": "https://shahroz869.github.io/csb-c0oogp/static/js/2.93b78f3f.chunk.js"
+  },
+  {
+    "revision": "2e3f48364264bcc07cb9",
+    "url": "https://shahroz869.github.io/csb-c0oogp/static/js/main.9d0531c0.chunk.js"
+  },
+  {
+    "revision": "f5e7aaa83e1105c0cd0e",
+    "url": "https://shahroz869.github.io/csb-c0oogp/static/js/runtime~main.bd030a56.js"
+  }
+]);
